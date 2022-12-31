efa0284442cc489a1c374ff66d190471cd981b11
MuratÇamurlu <mcamurlu1923@gmailcom>
2022-12-31 15:21:18 +0300

    postman_token

--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,10 @@
 
-
+from rest_framework.authtoken import views
 from .views import RegisterView
 from django.urls import path
 
 urlpatterns = [
    
     path("register/",RegisterView.as_view() ),
+    path("login/",views.obtain_auth_token ),
 ]
